5f4c38ba1e30bc6cf65d808c35d7b464fa0a0c0b
manan sahni <manansahni259@gmail.com>
2025-05-06 13:35:27 +0530

    Added sorting programs

---
+++ selectionsort.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int size,i,j,m,loc;
+    cout << "ENTER THE SIZE OF AN ARRAY : ";
+    cin >> size;
+    int arr[size];
+    cout << "ENTER HE VALUES OF AN ARRAY : ";
+    for(i=0; i <size ; i++){
+        cin >> arr[i];
+    }
+    for(i=0; i<size; i++){
+        m=arr[i];
+        loc=i+1;
+        for(j=i+1; j<size; j++){
+            
+        }
+        }
+}
