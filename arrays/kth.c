04f10492fb8682aafdb6da75eeab20d39c6045b9
manan sahni <manansahni259@gmail.com>
2025-05-06 14:12:19 +0530

    this is the array programs

---
+++ arrays/kth.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void insertion(int [arr],k,n,else){
+    if (k < 0  || k>n){}
+    printf"error and exit !!.");
+int main () {
+    int i,n,k,ele;
+    printf("enter the size of an array:"); 
+    scanf("%d", &n);
+    int arr[n];
+    printf("enter the values in an array:");
+    for(i=0; i<n ; i++) {
+        scanf("%d", &arr[i]);
+
+    }int num;
+    printf("enter the eleent to insert ");
+    scanf("%d" , &k);
+    arr[k]=num;
+    for(i=0 ; i<n ; i++){
+        printf("%d", arr[i]);
+
+    }return 0;
+}
