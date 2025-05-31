b1f174f233b32c6ccf6395b09769092f69dbef9e
manan sahni <manansahni259@gmail.com>
2025-05-31 22:07:52 +0530

    this is the function overloadig commit

---
+++ function_overloading.cpp/function_overloading.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int sum(float a, int b) {
+    cout << "Using function with 2 arguments (float, int)" << endl;
+    return a + b;
+}
+
+int sum(int a, int b, int c) {
+    cout << "Using function with 3 arguments (int, int, int)" << endl;
+    return a + b + c;
+}
+
+int main() {
+    cout << "The sum of 3 and 6 is " << sum(3, 6) << endl;
+    cout << "The sum of 3, 7 and 6 is " << sum(3, 7, 6) << endl;
+    return 0;
+}
